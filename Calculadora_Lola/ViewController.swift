a6448c0e35b662ac3d7aef3f5e042e83fac40a53
BRUNO GIBRAN REYES ALVAREZ <al205757@alumnos.uacj.mx>
2024-09-02 09:32:06 -0600

    Agregamos los archivos en Git

--- Calculadora_Lola/ViewController.swift
+++ Calculadora_Lola/ViewController.swift
@@ -14,6 +14,13 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet weak var btn_para_interactuar: UIButton!
+    
+    @IBOutlet weak var texto_a_cambiar: UILabel!
+    
+    
+    @IBAction func que_hacer_pushar_btn(_ sender: Any) {
+        texto_a_cambiar.text = "Bienvenidos a Himalaya"
+    }
 }
 
