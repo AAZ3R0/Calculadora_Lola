1d173362dba29e8371570be50f60b13456d82055
BRUNO GIBRAN REYES ALVAREZ <al205757@alumnos.uacj.mx>
2024-09-06 10:37:04 -0600

    [AGREGA] un diccionario

--- Calculadora_Lola/ViewController.swift
+++ Calculadora_Lola/ViewController.swift
@@ -13,16 +13,33 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    func InicializarCalculadora(){
+        
+    }
+    
+    var botones_interfaz: Dictionary<String, IUBotonCalculadora> = [:]
 
-    @IBOutlet weak var btn_para_interactuar: UIButton!
+    
     
     @IBOutlet weak var texto_a_cambiar: UILabel!
     
+    /// Description
+    /// - Parameter sender: sender description
     
     @IBAction func que_hacer_pushar_btn(_ sender: UIButton) {
         
-        texto_a_cambiar.text = sender.titleLabel!.text ?? "Nosupe"
-        
+    }
+    
+    
+    func inicializar_calculadora() -> Void{
+        crear_arreglo_botones()
+    }
+    
+    func crear_arreglo_botones() -> Void{
+        for numero in 0...9{
+            botones_interfaz["btn_\(numero)"] = IUBotonCalculadora("btn_\(numero)", numero: Character("\(numero)"), operacion: "")
+        }
     }
 }
 
