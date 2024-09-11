5aea6cc21e4afbe8cc6b2707a12ad343d572b2fd
BRUNO GIBRAN REYES ALVAREZ <al205757@alumnos.uacj.mx>
2024-09-11 10:38:13 -0600

    [AGREGA] funcion de aparecer operaciones

--- Calculadora_Lola/Modelo/Interfaz/InterfazBotones.swift
+++ Calculadora_Lola/Modelo/Interfaz/InterfazBotones.swift
@@ -6,36 +6,35 @@
 //
 
 import Foundation
+import UIKit
 
 // Interfaz Usuario [Nombre de la clase o estructura]
+// Interfaz Usuario [Nombre de la clase o estructura]
 struct IUBotonCalculadora {
-    var restorationID: String
+    var referencia_a_boton_interfaz: UIButton?
     var numero: Character
     var operacion: String
-    
-    init(_ id: String, numero: Character, operacion: String) {
-        self.restorationID = id
+    init(numero: Character, operacion: String) {
+        self.referencia_a_boton_interfaz = nil
         self.numero = numero
         self.operacion = operacion
     }
-    
     static func crear_arreglo_botones() -> Dictionary<String, IUBotonCalculadora> {
         var botones_interfaz: Dictionary<String, IUBotonCalculadora> = [:]
-        botones_interfaz["btn_0"] = IUBotonCalculadora("btn_0", numero: "0", operacion: "")
-        botones_interfaz["btn_1"] = IUBotonCalculadora("btn_1", numero: "1", operacion: "+")
-        botones_interfaz["btn_2"] = IUBotonCalculadora("btn_2", numero: "2", operacion: "")
-        botones_interfaz["btn_3"] = IUBotonCalculadora("btn_3", numero: "4", operacion: "")
-        botones_interfaz["btn_4"] = IUBotonCalculadora("btn_4", numero: "4", operacion: "")
-        botones_interfaz["btn_5"] = IUBotonCalculadora("btn_5", numero: "5", operacion: "")
-        botones_interfaz["btn_6"] = IUBotonCalculadora("btn_6", numero: "6", operacion: "")
-        botones_interfaz["btn_7"] = IUBotonCalculadora("btn_7", numero: "7", operacion: "*")
-        botones_interfaz["btn_8"] = IUBotonCalculadora("btn_8", numero: "8", operacion: "")
-        botones_interfaz["btn_9"] = IUBotonCalculadora("btn_9", numero: "9", operacion: "/")
+
+ 
+        botones_interfaz["btn_0"] = IUBotonCalculadora(numero: Character("0"), operacion: "")
+        botones_interfaz["btn_1"] = IUBotonCalculadora(numero: Character("1"), operacion: "+")
+        botones_interfaz["btn_2"] = IUBotonCalculadora(numero: Character("2"), operacion: "")
+        botones_interfaz["btn_3"] = IUBotonCalculadora(numero: Character("3"), operacion: "-")
+        botones_interfaz["btn_4"] = IUBotonCalculadora(numero: Character("4"), operacion: "")
+        botones_interfaz["btn_5"] = IUBotonCalculadora(numero: Character("5"), operacion: "")
+        botones_interfaz["btn_6"] = IUBotonCalculadora(numero: Character("6"), operacion: "")
+        botones_interfaz["btn_7"] = IUBotonCalculadora(numero: Character("7"), operacion: "*")
+        botones_interfaz["btn_8"] = IUBotonCalculadora(numero: Character("8"), operacion: "")
+        botones_interfaz["btn_9"] = IUBotonCalculadora(numero: Character("9"), operacion: "/")
         return botones_interfaz
     }
-    
-    
-    
 }
 
 
--- Calculadora_Lola/ViewController.swift
+++ Calculadora_Lola/ViewController.swift
@@ -7,14 +7,21 @@
 
 import UIKit
 
+enum estados_de_la_calculadora{
+    case seleccionar_numeros
+    case escoger_operacion
+    case mostrar_resultado
+}
+
 class ViewController: UIViewController {
-    
+    var estado_actual: estados_de_la_calculadora = estados_de_la_calculadora.seleccionar_numeros
     
     @IBOutlet weak var texto_a_cambiar: UILabel!
     @IBOutlet weak var boton_operacion: UIButton!
-    var botones_interfaz: Dictionary<String, IUBotonCalculadora> = [:]
+    @IBOutlet weak var vista_stack: UIStackView!
     
-   
+    var botones_interfaz: Dictionary<String, IUBotonCalculadora> = [:]
+    var operacion_actual: String? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,21 +33,80 @@ class ViewController: UIViewController {
     /// - Parameter sender: sender description
     
     @IBAction func que_hacer_pushar_btn(_ sender: UIButton) {
-        let text_a_añadir = botones_interfaz[(sender.restorationIdentifier ?? boton_operacion.restorationIdentifier) ?? "boton"]?.numero
-                texto_a_cambiar.text = "\(texto_a_cambiar.text ?? "")\(text_a_añadir!)"
+        if(estado_actual == estados_de_la_calculadora.seleccionar_numeros){
+            let text_a_añadir = botones_interfaz[(sender.restorationIdentifier ?? boton_operacion.restorationIdentifier) ?? "boton"]?.numero
+            
+            texto_a_cambiar.text = "\(texto_a_cambiar.text ?? "")\(text_a_añadir!)"
+        }
+        else if (estado_actual == estados_de_la_calculadora.escoger_operacion){
+            if let _mensajero: UIButton? = sender{
+                operacion_actual = botones_interfaz[_mensajero!.restorationIdentifier ?? "btn_0"]?.operacion
             }
+            else {
+                operacion_actual = nil
+            }
+        }
+        
+        dibujar_numeros_u_operaciones_en_interfaz()
+    }
+    
+    @IBAction func boton_escoger_operacion_pulsado(_ sender: UIButton) {
+        boton_operacion.setTitle("Ñ", for: .normal)
+        if (estado_actual == estados_de_la_calculadora.seleccionar_numeros){
+            estado_actual = estados_de_la_calculadora.escoger_operacion
+            dibujar_numeros_u_operaciones_en_interfaz()
+        }
+    }
+    
+    func identificar_botones(){
+        //for componente in self.view.subviews{
+        for pila_de_componentes in vista_stack.subviews{
+            for boton in pila_de_componentes.subviews{
+                if let identificador = boton.restorationIdentifier{
+                    print(botones_interfaz[identificador])
+                    botones_interfaz[identificador]?.referencia_a_boton_interfaz = boton as? UIButton
+                }
+            }
+        }
+        
+        for elemento in botones_interfaz.values{
+            print(elemento.referencia_a_boton_interfaz?.restorationIdentifier)
+            elemento.referencia_a_boton_interfaz?.setTitle("Ñ", for: .normal)
+        }
+    }
     
     func inicializar_calculadora() -> Void{
         crear_arreglo_botones()
+        identificar_botones()
     }
-
+    
+    
+    
+    
     func crear_arreglo_botones(){
         botones_interfaz = IUBotonCalculadora.crear_arreglo_botones()
     }
+    
+    func dibujar_numeros_u_operaciones_en_interfaz(){
+        if(estado_actual == estados_de_la_calculadora.escoger_operacion){
+            for elemento in botones_interfaz.values{
+                print(elemento.referencia_a_boton_interfaz?.restorationIdentifier)
+                elemento.referencia_a_boton_interfaz?.setTitle(elemento.operacion, for: .normal)
+            }
+        }
+        else if (estado_actual == estados_de_la_calculadora.seleccionar_numeros){
+            for elemento in botones_interfaz.values{
+                elemento.referencia_a_boton_interfaz?.setTitle(String(elemento.numero), for: .normal)
+            }
+        }
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
 }
-    
-    
-    
-    
-
-
